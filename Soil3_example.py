ac19c448824cb9d8ebdcf340f7d2d5db0183d837
Trung-Hieu Mai <mai.trunghieu@yahoo.com>
2017-07-31 13:11:45 +0200

    Soil3 change variable name

--- Soil3_example.py
+++ Soil3_example.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # Simulation of 3x3 wheat root systems development in 120 days
-# Distance betweem root system is 40 cm
-# Simulation output Root leng density [cm/cm3] (variable name RLD) and growing rate [cm/day] (variable name RLD_change) at each time step 
+# Distance betweem root systems is 40 cm
+# Simulation output Root leng density [cm/cm3] (variable name RLD) and growing rate [cm/cm3/day] (variable name RLD_change) at each time step 
 # created by Mai t.mai@jz-juelich.de
 # 31 July 2017
 
@@ -30,11 +30,11 @@ depth=140; # soil depth [cm]
 nl = 15; # number of layers
 
 RLD_lastTime=vRLD=np.zeros((nl)) # Root length density at last time step [cm/cm3]
+#simulation for each time step 
 for t in range(0,simtime):
 	for rs in allRS:
 	    rs.simulate(dt)
 	# Compute vertical RLD distribution in layers
-	#
 	vRLD=np.zeros((N*N,nl)); # N*N rows, nl columns
 	depth=150.;
 	c=0
@@ -45,7 +45,7 @@ for t in range(0,simtime):
 	      vRLD[c,:] /= (depth/nl)# rootLength per soil depth
 	      c += 1 # root system number
 	RLD=np.sum(vRLD,axis=0)/(Area*(depth/nl))
-	RLD_change = RLD-RLD_lastTime
+	growingRate = RLD-RLD_lastTime
 	RLD_lastTime = RLD
 	#Plot Root length density per layer
 	z=np.linspace(0,depth*(-1),nl)   # depth*-1 is the (negativ) z coordinate
@@ -58,7 +58,7 @@ for t in range(0,simtime):
 	plt.savefig(nameOutputFile+".png")
 	plt.close()
 	#Plot Growing rate per layer
-	plt.plot(RLD_change,z,'k-', color="blue",linewidth=2)
+	plt.plot(growingRate,z,'k-', color="blue",linewidth=2)
 	x1,x2,y1,y2 = plt.axis()
 	plt.axis((0,x2,y1,y2)) # set min of x axis to 0, because of some negative values of (mean-std)
 	plt.xlabel('Growing rate (cm/cm3/day)')
@@ -72,7 +72,7 @@ for t in range(0,simtime):
 	#np.savez(nameOutputFile, z=z, RLD=RLD)
 
 
-# Export results as single vtp files (as polylines)
+# Export root systems (for visualisation) as single vtp files (as polylines)
 c = 0
 ana = rb.SegmentAnalyser() # see example 3b
 for rs in allRS:
