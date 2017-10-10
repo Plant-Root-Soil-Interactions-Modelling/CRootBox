9eda535ccb5d9f03695cc95798bacc0cf5c9087f
Daniel Leitner <daniel.leitner@simwerk.at>
2017-10-10 14:56:16 +0200

    update

--- example4b_2.py
+++ example4b_2.py
@@ -7,9 +7,11 @@ rs = rb.RootSystem()
 name = "Anagallis_femina_Leitner_2010" 
 rs.openFile(name)
 
-soilprop = rb.SoilProperty1Dlinear(0,-50,100) # for root elongation 
-data = np.ones((100,))
-soilprop.setData(a2v(data))
+elongation_scaling = rb.SoilProperty1Dlinear(0,-50,100) # for root elongation 
+
+scales = np.ones((100,))
+
+elongation_scaling.setData(a2v(scales))
   
 # print("Value at -2 ", soilprop.getValue(rb.Vector3d(0,0,-2), r))
   
@@ -18,10 +20,9 @@ sigma = [0.4, 1., 1., 1., 1. ] * 2
 for i in range(0,10):  
     p = rs.getRootTypeParameter(i+1)
     p.dx = 0.25 # adjust resolution
-    p.tropismS = sigma[i] 
-     
+    p.tropismS = sigma[i]      
     # 1. Scale elongation
-    p.se = soilprop
+    p.se = elongation_scaling
        
 # simulation
 rs.initialize()
@@ -29,7 +30,14 @@ simtime = 120.
 dt = 1.
 N = 120/dt
 for i in range(0,round(N)):
+    
+    # update scales (e.g. from theta)
+    
+    # copy scales into scaling funciton
+    elongation_scaling.setData(a2v(scales))
+    
     rs.simulate(dt,True)
 
+rs.write("results/example_4b_2.vtp")  
 
 print("fin")
