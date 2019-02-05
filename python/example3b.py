rotation fixed

python/example3b.py
@@ -4,55 +4,55 @@ import matplotlib.pyplot as plt
 
 rs = rb.RootSystem()
 rs.openFile("Brassica_oleracea_Vansteenkiste_2014")
-rs.initialize() 
-rs.simulate(120, True) 
+rs.initialize()
+rs.simulate(120, True)
 
 # Soil core analysis
 r, depth, layers = 10, 100., 100
-soilcolumn = rb.SDF_PlantContainer(r,r,depth,False) # in the center of the root
-soilcolumn2 = rb.SDF_RotateTranslate(soilcolumn,0,0,rb.Vector3d(10,0,0)) # shift 10 cm
+soilcolumn = rb.SDF_PlantContainer(r, r, depth, False)  # in the center of the root
+soilcolumn2 = rb.SDF_RotateTranslate(soilcolumn, 0, 0, rb.Vector3d(10, 0, 0))  # shift 10 cm
 
 # pick one geometry for further analysis
 geom = soilcolumn
 
-z_=np.linspace(0,-1*depth,layers)
-fig, axes = plt.subplots(nrows=1, ncols=4, figsize=(16,8))
-for a in axes:    
+z_ = np.linspace(0, -1 * depth, layers)
+fig, axes = plt.subplots(nrows = 1, ncols = 4, figsize = (16, 8))
+for a in axes:
     a.set_xlabel('RLD (cm/cm)')
     a.set_ylabel('Depth (cm)')
 
 # Make a root length distribution
 ana = rb.SegmentAnalyser(rs)
-rl_ = ana.distribution(rb.ScalarType.length,0.,depth,layers,True)
+rl_ = ana.distribution(rb.ScalarType.length, 0., depth, layers, True)
 axes[0].set_title('All roots (120 days)')
-axes[0].plot(rl_,z_)
+axes[0].plot(rl_, z_)
 
 # Make a root length distribution along the soil core
 ana = rb.SegmentAnalyser(rs)
 ana.crop(geom)
 ana.pack()
-rl_ = ana.distribution(rb.ScalarType.length,0.,depth,layers,True)
+rl_ = ana.distribution(rb.ScalarType.length, 0., depth, layers, True)
 axes[1].set_title('Soil core (120 days)')
-axes[1].plot(rl_,z_)
+axes[1].plot(rl_, z_)
 
 # How it looked after 30 days?
 ana = rb.SegmentAnalyser(rs)
-ana.filter(rb.ScalarType.time,0,30)
+ana.filter(rb.ScalarType.time, 0, 30)
 ana.crop(geom)
 ana.pack()
-rl_ = ana.distribution(rb.ScalarType.length,0.,depth,layers,True)
+rl_ = ana.distribution(rb.ScalarType.length, 0., depth, layers, True)
 axes[2].set_title('Soil core (30 days)')
-axes[2].plot(rl_,z_)
+axes[2].plot(rl_, z_)
 
 # Only laterals?
 ana = rb.SegmentAnalyser(rs)
-ana.filter(rb.ScalarType.type,2) # assuming laterals are of type 2
+ana.filter(rb.ScalarType.type, 2)  # assuming laterals are of type 2
 ana.crop(geom)
 ana.pack()
-rl_ = ana.distribution(rb.ScalarType.length,0.,depth,layers,True)
+rl_ = ana.distribution(rb.ScalarType.length, 0., depth, layers, True)
 axes[3].set_title('Soil core, lateral roots (120 days)')
-axes[3].plot(rl_,z_)
+axes[3].plot(rl_, z_)
 
 fig.subplots_adjust()
-plt.savefig("results/example_3b.png")
+plt.savefig("../results/example_3b.png")
 plt.show()

python/example3d.py
@@ -4,72 +4,76 @@ from multiprocessing import Pool
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 # sets all standard deviation to value*s
-def set_all_sd(rs,s):
-    for i in range(0,10):
-        p = rs.getRootTypeParameter(i+1)
-        p.lbs = p.lb*s
-        p.las = p.la*s
-        p.lns = p.ln*s
-        p.nobs = p.nob*s
-        p.rs = p.r*s
-        p.a_s = p.a*s         
+def set_all_sd(rs, s):
+    for i in range(0, 10):
+        p = rs.getRootTypeParameter(i + 1)
+        p.lbs = p.lb * s
+        p.las = p.la * s
+        p.lns = p.ln * s
+        p.nobs = p.nob * s
+        p.rs = p.r * s
+        p.a_s = p.a * s
+
 
 # Parameters
-name = "Triticum_aestivum_a_Bingham_2011" 
+name = "Triticum_aestivum_a_Bingham_2011"
 simtime = 20
-N = 50 # resolution of paramter
-runs = 10 # iterations 
-theta0_ = np.linspace(0,math.pi/2,N)
+N = 50  # resolution of paramter
+runs = 10  # iterations
+theta0_ = np.linspace(0, math.pi / 2, N)
+
 
 # One simulation
 def simulate(i):
-    rs = rb.RootSystem()    
+    rs = rb.RootSystem()
     rs.openFile(name)
-    set_all_sd(rs,0.) # set all sd to zero                
+    set_all_sd(rs, 0.)  # set all sd to zero
 
     # vary parameter
-    p1 = rs.getRootTypeParameter(1) # tap root
-    p4 = rs.getRootTypeParameter(4) # basal roots
+    p1 = rs.getRootTypeParameter(1)  # tap root
+    p4 = rs.getRootTypeParameter(4)  # basal roots
     p1.theta = theta0_[i]
-    p4.theta = theta0_[i]   
+    p4.theta = theta0_[i]
 
     # simulation
-    rs.initialize()     
-    rs.simulate(simtime, True) 
+    rs.initialize()
+    rs.simulate(simtime, True)
 
-    # target 
+    # target
     roots = rs.getPolylines()
     depth = 0.
     rad_dist = 0.
     for r in roots:
         depth += r[-1].z
-        rad_dist += math.hypot(r[-1].x,r[-1].y)        
+        rad_dist += math.hypot(r[-1].x, r[-1].y)
     depth /= len(roots)
     rad_dist /= len(roots)
 
     return depth, rad_dist
 
+
 depth_ = np.zeros(N)
 rad_dist_ = np.zeros(N)
 
-for r in range(0,runs):    
-    
+for r in range(0, runs):
+
     # Parallel execution
-    param = [] # param is a list of tuples
-    for i in range(0,N):
-        param.append((i,))    
+    param = []  # param is a list of tuples
+    for i in range(0, N):
+        param.append((i,))
     pool = Pool()
-    output =  pool.starmap(simulate,param)
+    output = pool.starmap(simulate, param)
     pool.close()
 
     # Copy results
-    for i,o in enumerate(output):
-        depth_[i] += (o[0]/runs)
-        rad_dist_[i] += (o[1]/runs)
-    
-# Figure 
-fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(10,8))
+    for i, o in enumerate(output):
+        depth_[i] += (o[0] / runs)
+        rad_dist_[i] += (o[1] / runs)
+
+# Figure
+fig, axes = plt.subplots(nrows = 1, ncols = 2, figsize = (10, 8))
 axes[0].set_xlabel('Insertion angle theta (-)')
 axes[1].set_xlabel('Insertion angle theta (-)')
 axes[0].set_ylabel('Mean tip depth (cm)')
@@ -77,6 +81,6 @@ axes[1].set_ylabel('Mean tip radial distance (cm)')
 axes[0].plot(theta0_, depth_)
 axes[1].plot(theta0_, rad_dist_)
 fig.subplots_adjust()
-plt.savefig("results/example_3d.png")
+plt.savefig("../results/example_3d.png")
 plt.show()
 